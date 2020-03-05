3aa61fafcba764da22eb348bf86d1adc08692a58
D.Gorchakov <D.Gorchakov@alfacapital.ru>
2020-03-05 15:34:13 +0300

    first commit

---
+++ JavaNIO/src/DemoNIO.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class DemoNIO {
+
+    public static void main(String[] args) throws IOException {
+
+        // http://tutorials.jenkov.com/java-nio/buffers.html
+
+    }
+
+
+    private void channelExample(String pathToFile) throws IOException {
+        RandomAccessFile aFile = new RandomAccessFile(pathToFile, "rw");
+        FileChannel inChannel = aFile.getChannel();
+
+        ByteBuffer buf = ByteBuffer.allocate(48);
+
+        int bytesRead = inChannel.read(buf);
+        while (bytesRead != -1) {
+
+            System.out.println("Read " + bytesRead);
+            buf.flip();
+
+            while (buf.hasRemaining()) {
+                System.out.print((char) buf.get());
+            }
+            buf.clear();
+            bytesRead = inChannel.read(buf);
+        }
+        aFile.close();
+    }
+}
